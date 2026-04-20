22d2c41be7452132632106b15fc05ecf7a855357
rapchiktech2 <onlinesite45@gmail.com>
2026-04-20 11:04:06 +0530

    Update build.gradle.kts

--- KatMovieHD/build.gradle.kts
+++ KatMovieHD/build.gradle.kts
@@ -1,10 +1,10 @@
-version = 1
+apply(plugin = "com.lagradost.cloudstream3.gradle")
 
 cloudstream {
-    description = "Hollywood, K-Drama, Anime, Netflix, Disney+ Hindi Dubbed from KatMovieHD"
-    authors      = listOf("rapchiktech")
-    status       = 1
-    language     = "hi"
-    tvTypes      = listOf("Movie", "TvSeries", "AsianDrama", "Anime")
-    iconUrl      = "https://new.katmoviehd.cymru/favicon.ico"
+    description = "Hollywood, K-Drama, Anime, Netflix, Disney+ from KatMovieHD"
+    authors = listOf("rapchiktech")
+    status = 1
+    language = "hi"
+    tvTypes = listOf("Movie", "TvSeries", "AsianDrama", "Anime")
+    iconUrl = "https://new.katmoviehd.cymru/favicon.ico"
 }
