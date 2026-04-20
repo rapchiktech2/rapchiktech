4d6fb9469a1fde574c6884db6652a9522736c2fb
rapchiktech2 <onlinesite45@gmail.com>
2026-04-20 10:29:52 +0530

    Create build.gradle.kts

---
+++ KatMovieHD/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Hollywood, K-Drama, Anime, Netflix, Disney+ Hindi Dubbed from KatMovieHD"
+    authors      = listOf("rapchiktech")
+    status       = 1
+    language     = "hi"
+    tvTypes      = listOf("Movie", "TvSeries", "AsianDrama", "Anime")
+    iconUrl      = "https://new.katmoviehd.cymru/favicon.ico"
+}
