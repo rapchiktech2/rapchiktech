7dfe963cc410b3eca657c463c334ef00c7f1fbe1
rapchiktech2 <onlinesite45@gmail.com>
2026-04-20 10:24:42 +0530

    Create build.gradle.kts

---
+++ MkvDrama/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Asian dramas, movies and shows with English subtitles from MkvDrama"
+    authors      = listOf("rapchiktech")
+    status       = 1
+    language     = "en"
+    tvTypes      = listOf("AsianDrama", "Movie")
+    iconUrl      = "https://mkvdrama.net/favicon.ico"
+}
